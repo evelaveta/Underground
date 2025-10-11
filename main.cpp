b5aefcd783f27016aa0f70ee4ec05cae8ed76fe7
evelaveta <iluhina794@gmail.com>
2025-10-11 17:05:47 +0300

    round 2.3
    tests

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <cassert>
-#include "base_station.h"
+#include "usual_station.h"
+#include "transfer_station.h"
+#include "cross_platform_station.h"
+#include "span.h"
+#include "line.h"
 
 int main() {
     BaseStation* station1 = new BaseStation("Station 1", 1000, 1); // назваине, траффик, линия
@@ -25,6 +28,37 @@ int main() {
     delete station1;
     delete station2;
     delete station3;
+
+    Line line(1, "Green Circle", true);
+
+    BaseStation s1("S1", 80, 1);
+    BaseStation s2("S2", 90, 1);
+    BaseStation s3("S3", 100, 1);
+    BaseStation s4("S4", 110, 1);
+
+    line.AddStation(&s1);
+    line.AddStation(&s2);
+    line.AddStation(&s3);
+    line.AddStation(&s4);
+
+    line.AddSpan(&s1, &s2, 2);
+    line.AddSpan(&s2, &s3, 3);
+    line.AddSpan(&s3, &s4, 4);
+    line.AddSpan(&s4, &s1, 1);
+
+    line.stationIndices_[&s1] = 0;
+    line.stationIndices_[&s2] = 1;
+    line.stationIndices_[&s3] = 2;
+    line.stationIndices_[&s4] = 3;
+
+    int travelTimeClockwise = line.GetTravelTime(&s1, &s3);
+    int travelTimeCounter = line.GetTravelTime(&s3, &s1);
+
+    assert(travelTimeClockwise == 5);
+    assert(travelTimeCounter == 5);
+
+    travelTimeClockwise = line.GetTravelTime(&s4, &s2);
+    assert(travelTimeClockwise == 3);
 
     return 0;
 }
